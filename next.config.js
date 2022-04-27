746985f8969d3bf0a65eac5f1e5b9415fca16bcf
sonu zelp <sonu@zelp.io>
2022-04-27 11:00:54 +0530

    pwa ready

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa');
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['via.placeholder.com'],
   },
-  experimental: { images: { layoutRaw: true } }
-}
+  experimental: { images: { layoutRaw: true } },
+  pwa: {
+    dest: 'public',
+    swSrc: 'service-worker.js',
+  },
+})
 
 module.exports = nextConfig
